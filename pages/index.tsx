6132ef491e6e8c1c56a8dd8fb067c2994f4a04a0
ducthanh <ducthanhpg@gmail.com>
2023-10-19 17:56:05 +0700

    init project

---
+++ pages/index.tsx
@@ -0,0 +1,69 @@
+import { ReactElement } from 'react';
+
+import { Breadcrumb, DatePicker, Steps } from 'antd';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import Home from '@components/Home';
+import MainLayout from '@layout/MainLayout';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Home />
+      <Breadcrumb
+        items={[
+          {
+            title: 'Home',
+          },
+          {
+            title: <a href=''>Application Center </a>,
+          },
+          {
+            title: <a href=''>Application List</a>,
+          },
+          {
+            title: 'An Application',
+          },
+        ]}
+      />
+      <Steps
+        current={1}
+        items={[
+          {
+            title: 'Finished',
+            description: 'asdsa',
+          },
+          {
+            title: 'In Progress',
+            description: 'asdsa',
+            subTitle: 'Left 00:00:08',
+          },
+          {
+            title: 'Waiting',
+            description: 'asdsa',
+          },
+        ]}
+      />
+      <DatePicker />
+    </div>
+  );
+};
+
+HomePage.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <MainLayout>
+      <>{page}</>
+    </MainLayout>
+  );
+};
+
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'home'])),
+      // Will be passed to the page component as props
+    },
+  };
+}
+
+export default HomePage;
---
+++ src/layout/MainLayout/index.tsx
@@ -0,0 +1,16 @@
+import dynamic from 'next/dynamic';
+
+const MainHeader = dynamic(() => import('../components/MainHeader'));
+const Footer = dynamic(() => import('../components/Footer'));
+
+const MainLayout = ({ children }: any) => {
+  return (
+    <>
+      <MainHeader />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
